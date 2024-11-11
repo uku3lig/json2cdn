f3f0a5e70cad2a14eb96f4d4c897c83498b46aeb
uku <hi@uku.moe>
2024-11-11 20:58:19 +0100

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    java
+}
+
+group = "net.uku3lig"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+}
---
+++ src/main/java/net/uku3lig/json2cdn/Main.java
@@ -0,0 +1,7 @@
+package net.uku3lig.json2cdn;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
