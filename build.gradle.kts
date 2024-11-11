29c6db776f4355c4a7173a2b720fca5e31b758b4
uku <hi@uku.moe>
2024-11-11 21:57:49 +0100

    add converter

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     java
+    id("com.gradleup.shadow") version "8.3.5"
 }
 
 group = "net.uku3lig"
@@ -7,7 +8,11 @@ version = "0.1.0"
 
 repositories {
     mavenCentral()
+    maven {
+        url = uri("https://repo.panda-lang.org/releases")
+    }
 }
 
 dependencies {
+    implementation("net.dzikoysk:cdn:1.14.5")
 }
--- src/main/java/net/uku3lig/json2cdn/Main.java
+++ src/main/java/net/uku3lig/json2cdn/Main.java
@@ -1,7 +1,30 @@
 package net.uku3lig.json2cdn;
 
+import net.dzikoysk.cdn.Cdn;
+import net.dzikoysk.cdn.CdnException;
+import net.dzikoysk.cdn.CdnFactory;
+import net.dzikoysk.cdn.source.Source;
+import panda.std.Result;
+
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        if (args.length == 0) {
+            System.err.println("usage: json2cdn [input file path]");
+            System.exit(1);
+        }
+
+        final Cdn json = CdnFactory.createJsonLike();
+        final Cdn cdn = CdnFactory.createStandard();
+        final File file = new File(args[0]);
+
+        final Result<String, CdnException> config = json.load(Source.of(file)).flatMap(cdn::render);
+
+        if (config.isOk()) {
+            System.out.println(config.get());
+        } else {
+            throw config.getError();
+        }
     }
 }
